c57a1a0a318cc2e2214492dd203bc1146d72cf47
Hanif Jameel <hanif.jameel@7digital.com>
2015-10-23 12:08:00 +0100

    Set up initial start point for kata.

--- AdventureGameKata/AdventureGame.cs
+++ AdventureGameKata/AdventureGame.cs
@@ -6,7 +6,13 @@ namespace AdventureGameKata
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hi");
+			Console.WriteLine("Hi, What is your name?");
+
+			var name = Console.ReadLine();
+
+			Console.WriteLine("Hi {0}", name);
+
+			Console.ReadLine();
 		}
 	}
 }
--- AdventureGameKataTests/Acceptance/AdventureGameTest.cs
+++ AdventureGameKataTests/Acceptance/AdventureGameTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using System.Text;
 using NUnit.Framework;
 
@@ -7,27 +8,63 @@ namespace AdventureGameKataTests.Acceptance
 	[TestFixture]
 	class AdventureGameTest
 	{
+		private const string NewLine = "\n";
+
 		[Test]
 		public void it_greets_the_new_player()
+		{
+			var game = GameProcess.Start();
+			game.Input.Write("Player" + NewLine);
+			game.Input.Write(NewLine);
+			game.WaitForExit();
+
+			Assert.That(game.ReadOutput(), Is.StringContaining("Hi, What is your name?"));
+			Assert.That(game.ReadOutput(), Is.StringContaining("Hi " + "Player"));
+		}
+	}
+
+	class GameProcess
+	{
+		private readonly Process _process;
+		private readonly StringBuilder _output;
+
+		private GameProcess(Process process, StringBuilder output)
+		{
+			_process = process;
+			_output = output;
+		}
+
+		private const string FileName = @"..\..\..\AdventureGameKata\bin\Debug\AdventureGameKata.exe";
+		public static GameProcess Start()
 		{
 			var process = new Process
 				{
 					StartInfo =
 						{
-							FileName = @"..\..\..\AdventureGameKata\bin\Debug\AdventureGameKata.exe",
+							FileName = FileName,
 							UseShellExecute = false,
-							RedirectStandardOutput = true
+							RedirectStandardOutput = true,
+							RedirectStandardInput = true
 						}
 				};
 
 			var output = new StringBuilder();
-			process.OutputDataReceived += (sender, args) => output.Append(args.Data);
-
+			process.OutputDataReceived += (sender, args) => output.Append(args.Data + "\n");
 			process.Start();
 			process.BeginOutputReadLine();
-			process.WaitForExit();
+			return new GameProcess(process, output);
+		}
 
-			Assert.That(output.ToString(), Is.StringContaining("Hi"));
+		public StreamWriter Input { get { return _process.StandardInput; } }
+
+		public void WaitForExit()
+		{
+			_process.WaitForExit();
+		}
+
+		public string ReadOutput()
+		{
+			return _output.ToString();
 		}
 	}
 }
